65af315960ca9d07c0f341ad819b99517cd692db
cur1576 <cur1576@gmx.de>
2019-03-02 15:15:59 +0100

    commit_02.03_03

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.mycalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnNumber0;
+    Button btnNumber1;
+    Button btnNumber2;
+    Button btnNumber3;
+    Button btnNumber4;
+    Button btnNumber5;
+    Button btnNumber6;
+    Button btnNumber7;
+    Button btnNumber8;
+    Button btnNumber9;
+
+    TextView txtResult;
+
+    EditText editInput;
+
+    Button btnCE;
+    Button btnC;
+    Button btnDelete;
+
+    Button btnAdd;
+    Button btnSub;
+    Button btnMul;
+    Button btnDiv;
+
+    Button btnDot;
+    Button btnResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNumber0 = (Button)findViewById(R.id.btnNumber0);
+
     }
 }
